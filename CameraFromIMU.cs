b73e66627d80dcf8039fd8a62d37cc8eb704c001
Tom Allen <tom@jugglethis.net>
2013-07-02 22:32:22 +1000

    Upgraded script to Tinkerforge Protocol 2.0

--- CameraFromIMU.cs
+++ CameraFromIMU.cs
@@ -7,7 +7,7 @@ public class CameraFromIMU : MonoBehaviour {
 	
 	private static string _host = "localhost";
 	private static int _port = 4223;
-	private static string _uid = "a4JritAp6Go"; // Change to your UID
+	private static string _uid = "6wUjm3"; // Change to your UID
 
 	private static BrickIMU _imu;
 	private static IPConnection _ipcon;
@@ -16,20 +16,26 @@ public class CameraFromIMU : MonoBehaviour {
 	private static Quaternion _qOrigin = new Quaternion(0.0f,0.0f,0.0f,1.0f);
 	private static Quaternion _qPrevious = new Quaternion(0.0f,0.0f,0.0f,1.0f);
 	
-	public float _speed = 100.0f;
+	public float _speed = 100.0f; // Forwards speed.
+	public float _maxStrafeVelocity = 100.0f; // Max speed left and right.
+	public float _strafeKeyProportion = 0.0f; // Current left/right key press proportion (-ve = left, +ve = right).
+	public float _updownKeyProportion = 0.0f; // Current up/down key press proportion.
+	public float _rotateLeftRightKeyAngle = 0.0f; // Current angular rotation around world's up axis caused by Q/E keys.
+	public float _maxLiftVelocity = 25.0f; // Velocity upwards when up is pressed (eventually, when left and right are down).
+	public float _maxFallVelocity = 50.0f; // Velocity downwards when pressing downwards.
 	
 	// Use this for initialization
 	void Start () {
-		_ipcon = new IPConnection(_host, _port); // Create connection to brickd
-		_imu = new BrickIMU(_uid); // Create device object
-		_ipcon.AddDevice(_imu); // Add device to IP connection
+		_ipcon = new IPConnection(); // Create connection to brickd
+		_imu = new BrickIMU(_uid, _ipcon); // Create device object
+		_ipcon.Connect(_host,_port); // Add device to IP connection
 		// Don't use device before it is added to a connection
 
 		// Set period for quaternion callback to 10ms
 		_imu.SetQuaternionPeriod(10);
 
 		// Register quaternion callback to QuaternionCB
-		_imu.RegisterCallback(new BrickIMU.Quaternion(QuaternionCB));
+		_imu.Quaternion += QuaternionCB;
 		
 		// Set rotational origin to however the IMU is pointing at the start.
 		float x,y,z,w;
@@ -44,7 +50,7 @@ public class CameraFromIMU : MonoBehaviour {
 	}
 	
 	// Quaternion callback
-	static void QuaternionCB(float x, float y, float z, float w)
+	static void QuaternionCB(BrickIMU sender, float x, float y, float z, float w)
 	{
 		//Debug.Log("x: " + x + "\ny: " + y + "\nz: " + z + "\nw: " + w + "\n");
 		_qPrevious = _q; // Save q to qPrevious
@@ -55,7 +61,7 @@ public class CameraFromIMU : MonoBehaviour {
 	}
 	
 	void OnApplicationExit() {
-		_ipcon.Destroy();
+		_ipcon.Disconnect();
 	}
 	
 	void OnGUI() {
@@ -87,6 +93,40 @@ public class CameraFromIMU : MonoBehaviour {
 		transform.Rotate( Vector3.forward * q.eulerAngles.y, Space.World );
 		transform.Rotate( Vector3.up * q.eulerAngles.z, Space.World );
 		
+		// Strafe according to left/right keys.
+		//_strafeKeyProportion = Input.GetAxis("Horizontal");
+		if (Input.GetKey (KeyCode.A))
+			_strafeKeyProportion = -1.0f;
+		else if (Input.GetKey (KeyCode.D))
+			_strafeKeyProportion = 1.0f;
+        else
+			_strafeKeyProportion = 0.0f;
+		transform.Translate( Vector3.right * _strafeKeyProportion * _maxStrafeVelocity * Time.deltaTime );
+		
+		// Rise/fall (w.r.t. to the *world*'s z-axis) according to up/down keys.
+		//_updownKeyProportion = Input.GetAxis("Vertical");
+		if (Input.GetKey (KeyCode.W))
+			_updownKeyProportion = 1.0f;
+		else if (Input.GetKey (KeyCode.S))
+			_updownKeyProportion = -1.0f;
+        else
+			_updownKeyProportion = 0.0f;
+		if( _updownKeyProportion > 0.0f )
+		{
+			transform.Translate( Vector3.up * _updownKeyProportion * _maxLiftVelocity * Time.deltaTime, Space.World );
+		}
+		else
+		{
+			transform.Translate( Vector3.up * _updownKeyProportion * _maxFallVelocity * Time.deltaTime, Space.World );
+		}
+		
+		// Rotate around up world's up axis by Q/E keys.
+		if (Input.GetKey (KeyCode.Q))
+			_rotateLeftRightKeyAngle = (_rotateLeftRightKeyAngle-1.0f) % 360.0f;
+		else if (Input.GetKey (KeyCode.E))
+			_rotateLeftRightKeyAngle = (_rotateLeftRightKeyAngle+1.0f) % 360.0f;
+		transform.Rotate( Vector3.up * _rotateLeftRightKeyAngle, Space.World );
+			
 		// Fly forwards.
 		transform.Translate(Vector3.forward * Time.deltaTime * _speed);
 	}
